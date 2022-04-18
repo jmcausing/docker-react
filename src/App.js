09264f0c22fd1d232fb715df3f55daf06a11cc2d
jmcausing <johnmarkcausing@gmail.com>
2022-04-18 17:01:22 +0800

    Changed test text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Bye xXXxx therexxx! sad asd</p>
+        <p> Bye !</p>
         <a
           className="App-link"
           href="https://reactjs.org"
