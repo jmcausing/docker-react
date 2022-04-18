42f3b749d75d2bbed3886fd236f1ad2c31453ddd
jmcausing <johnmarkcausing@gmail.com>
2022-04-18 18:30:16 +0800

    fixed duplicate function in app.test.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is a feature branch! xx
+          Learn React
         </a>
       </header>
     </div>
