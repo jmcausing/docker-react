7242c53ac62140cc0af9b9d735fb03cd6d303a09
jmcausing <johnmarkcausing@gmail.com>
2022-04-18 18:38:32 +0800

    text changed by feature

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p> Hello John Mark! This is your first React app! Changed by master</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. This is from feature branch
         </a>
       </header>
     </div>
